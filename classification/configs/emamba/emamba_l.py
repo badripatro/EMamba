589138b07b5d2df207305b7f9c0260d4a55c91c6
Badri Narayana Patro <patrobadri.iitb@gmail.com>
2024-12-14 23:26:09 +0530

    Add files via upload

---
+++ classification/configs/emamba/emamba_l.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+cfg = dict(
+    model='emamba_l',
+    drop_path=0.3,
+    clip_grad=1.0,
+    output_dir='checkpoints/emamba_l',
+)
---
+++ classification/configs/emamba/emamba_s.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+cfg = dict(
+    model='emamba_s',
+    drop_path=0.1,
+    clip_grad=None,
+    output_dir='checkpoints/emamba_s',
+)
